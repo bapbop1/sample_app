d19ceded6dd117cdb4d616ed20f732431834fd58
hungxuan <nguyen.xuan.hung@framgia.com>
2018-05-08 10:11:25 +0700

    Update file routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,14 +21,11 @@
 #   get  '/contact', to: 'static_pages#contact'
 # end
 Rails.application.routes.draw do
-<<<<<<< HEAD
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-=======
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
 end
->>>>>>> filling-in-layout
+
